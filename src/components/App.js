55c24d60d63e959d109b2a33cec0f3dab189eb9d
lleonardus <leonardus.wizard@gmail.com>
2024-01-09 16:32:36 -0300

    changing page width to 90rem

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,10 @@ import { Header } from "./Header";
 
 export default function App() {
   return (
-    <div className="p-5">
-      <Header />
+    <div className="flex justify-center p-5">
+      <div className="w-[90rem]">
+        <Header />
+      </div>
     </div>
   );
 }

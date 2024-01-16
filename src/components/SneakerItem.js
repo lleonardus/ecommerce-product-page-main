1cd543626efe0aaa4da0b64acc93c60435277928
lleonardus <leonardus.wizard@gmail.com>
2024-01-16 17:05:00 -0300

    decreasing outline width on SneakerItem to 3px

--- src/components/SneakerItem.js
+++ src/components/SneakerItem.js
@@ -5,8 +5,9 @@ export function SneakerItem({ sneakerId, selectedSneaker, setSneakerId }) {
     <li
       onClick={() => setSneakerId(sneakerId)}
       className={`relative cursor-pointer overflow-hidden rounded-2xl hover:after:absolute hover:after:inset-0 hover:after:bg-white/50 ${
-        isSelectedSneaker &&
-        "outline outline-[4px] outline-orange-200 after:absolute after:inset-0 after:bg-white/70 "
+        isSelectedSneaker
+          ? "outline outline-[3px] outline-orange-200 after:absolute after:inset-0 after:bg-white/70 "
+          : ""
       }`}
     >
       <img

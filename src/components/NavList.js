f224de150a41948ba6b08ba29630862959473ba7
lleonardus <leonardus.wizard@gmail.com>
2024-01-16 16:40:59 -0300

    creating animated transition for the mobile menu

--- src/components/NavList.js
+++ src/components/NavList.js
@@ -3,11 +3,15 @@ import { NavItem } from "./NavItem";
 export function NavList({ isMenuOpen }) {
   return (
     <nav
-      className={`fixed inset-0 z-[1000] bg-black/75  md:static md:block ${
-        isMenuOpen ? "" : "hidden"
+      className={`transition-colors md:static md:block ${
+        isMenuOpen ? "fixed inset-0 z-[1000] bg-black/75" : "bg-[transparent]"
       }`}
     >
-      <div className="fixed left-0 right-[40%] h-screen bg-white md:static md:h-max">
+      <div
+        className={`fixed left-0 right-[40%] h-screen bg-white transition-transform md:static md:h-max md:translate-x-0 ${
+          isMenuOpen ? "" : "-translate-x-full"
+        }`}
+      >
         <ul className="mt-24 flex flex-col gap-5 pl-5 md:mt-0 md:flex-row md:pl-0">
           <NavItem link="#">Collections</NavItem>
           <NavItem link="#">Men</NavItem>

7972c769e8d02198a3d7b0d43eb7c19d40036405
lleonardus <leonardus.wizard@gmail.com>
2024-01-16 17:23:57 -0300

    improving the responsiveness of component Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,16 +12,16 @@ export function Main({ numberOfSneakers, setNumberOfSneakers }) {
   return (
     <main className="md:mt-20 md:grid md:grid-cols-2 md:gap-5 xl:px-10">
       <Gallery />
-      <div className="px-5 pt-5  md:max-w-[55ch] lg:pt-16">
+      <div className="px-5 pt-5 md:max-w-[55ch] lg:pt-16">
         <div className="flex flex-col gap-2">
           <h2 className="text-sm font-bold tracking-widest text-orange-200">
             SNEAKER COMPANY
           </h2>
-          <h1 className="text-3xl font-bold text-blue-900 md:text-5xl">
+          <h1 className="text-3xl font-bold text-blue-900 md:text-4xl lg:text-5xl">
             Fall Limited Edition Sneakers
           </h1>
         </div>
-        <p className="py-5 text-blue-800 md:pb-7 md:pt-10">
+        <p className="py-5 text-blue-800 md:pb-7 md:pt-7 lg:pt-10">
           These low-profile sneakers are your perfect casual wear companion.
           Featuring a durable rubber outer sole, they'll withstand everything
           the weather can offer.

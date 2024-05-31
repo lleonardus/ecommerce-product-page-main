0016fc7a31d3b904582e4e7e1732427c3c6d6361
lleonardus <leonardus.wizard@gmail.com>
2024-05-31 19:10:06 -0300

    adding colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      black: "hsl(0, 0%, 0%)",
+      orange: {
+        100: "hsl(25, 100%, 94%)",
+        200: "hsl(26, 100%, 55%)",
+      },
+      blue: {
+        100: "hsl(223, 64%, 98%)",
+        700: "hsl(220, 14%, 75%)",
+        800: "hsl(219, 9%, 45%)",
+        900: "hsl(220, 13%, 13%)",
+      },
+    },
     extend: {},
   },
   plugins: [],

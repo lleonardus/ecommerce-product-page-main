ccde4a3d961b51b68cc83046a5ce2d042d1205bc
lleonardus <leonardus.wizard@gmail.com>
2024-06-03 20:03:19 -0300

    adding selectedId and onClick properties to the Thubmnail component, and also adding hover effects and special classes for when a thumbnail is selected

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -1,6 +1,11 @@
-function Thumbnail({ index, className }) {
+function Thumbnail({ index, selectedId, onClick, className }) {
+  const isSelected = index === selectedId;
+
   return (
-    <li className={`overflow-hidden rounded-2xl ${className}`}>
+    <li
+      onClick={() => onClick(index)}
+      className={`relative cursor-pointer overflow-hidden rounded-xl after:inset-0 after:bg-white/65 hover:hover:hover:after:absolute ${className} ${isSelected ? "border-[2px] border-orange-200 after:absolute after:inset-0 after:bg-white/65" : ""}`}
+    >
       <img
         src={`images/image-product-${index}-thumbnail.jpg`}
         alt="sneaker thumbnail"

1620c6738bd87d2ae1eeb282f56c1dcc10e46bd3
lleonardus <leonardus.wizard@gmail.com>
2024-06-05 19:11:26 -0300

    removing the position property from ArrowButton and adding the className property to allow more flexibility

--- src/components/ArrowButton.jsx
+++ src/components/ArrowButton.jsx
@@ -1,8 +1,7 @@
-// position can be only 'right' or 'left'
-function ArrowButton({ position, onClick }) {
+function ArrowButton({ className, onClick }) {
   return (
     <div
-      className={`absolute bottom-[50%] grid aspect-square w-10 translate-y-[50%] cursor-pointer place-items-center rounded-full bg-white text-blue-900 hover:text-orange-200 ${position === "left" ? "left-4 rotate-180 lg:left-0 lg:translate-x-[-50%]" : "right-4 lg:right-0 lg:translate-x-[50%]"}`}
+      className={`absolute bottom-[50%] grid aspect-square w-10 translate-y-[50%] cursor-pointer place-items-center rounded-full bg-white text-blue-900 hover:text-orange-200 ${className}`}
       onClick={onClick}
     >
       <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">

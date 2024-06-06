7ad604e2ea56283f9d84dd6fea037c77b30464af
lleonardus <leonardus.wizard@gmail.com>
2024-06-06 06:20:11 -0300

    making CloseIcon svg scalable and setting its width to 1.25rem

--- src/components/CloseIcon.jsx
+++ src/components/CloseIcon.jsx
@@ -1,10 +1,14 @@
 function CloseIcon({ onClick }) {
   return (
     <div
-      className="absolute -top-10 right-0 w-min cursor-pointer text-white hover:text-orange-200"
+      className="absolute -top-10 right-0 aspect-square w-5 cursor-pointer text-white hover:text-orange-200"
       onClick={onClick}
     >
-      <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        className="h-auto w-full"
+        viewBox="0 0 14 15"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <path
           d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
           fill="currentColor"

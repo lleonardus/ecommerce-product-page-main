8eea92de3d89f731b9f2fe0ce3f24e953722991d
lleonardus <leonardus.wizard@gmail.com>
2024-06-07 14:22:10 -0300

    changing py-3 to py-4 in AddToCart and ItemQuantitySelector components

--- src/components/ItemQuantitySelector.jsx
+++ src/components/ItemQuantitySelector.jsx
@@ -6,7 +6,7 @@ function ItemQuantitySelector() {
   const { quantity } = useCart();
 
   return (
-    <div className="flex justify-between rounded-lg bg-blue-100 px-5 py-3 lg:w-[40%] lg:min-w-32 lg:py-4">
+    <div className="flex justify-between rounded-lg bg-blue-100 px-5 py-4 lg:w-[40%] lg:min-w-32">
       <MinusButton />
       <span className="font-bold text-blue-900">{quantity}</span>
       <PlusButton />
